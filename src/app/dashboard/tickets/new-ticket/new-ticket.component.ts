17c2045a5a5dbaa53d5426a163ef0ccbc28aa017
Qaiser <qaiser110@gmail.com>
2025-09-28 19:44:24 +1000

    Ticket Form/Component

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, output, ViewChild } from '@angular/core';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { ButtonComponent } from '../../../shared/button/button.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ControlComponent, ButtonComponent],
+  imports: [ControlComponent, ButtonComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {}
+export class NewTicketComponent {
+  @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+
+  // @Output() onAdd = new EventEmitter<{ title: string; text: string }>();
+  onAdd = output<{ title: string; text: string }>();
+
+  onSubmit(title: string, requestText: string) {
+    this.onAdd.emit({ title, text: requestText });
+    this.form?.nativeElement.reset();
+  }
+}
--- src/app/dashboard/tickets/ticket/ticket.component.ts
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { Ticket } from '../ticket.model';
 
 @Component({
   selector: 'app-ticket',
   standalone: true,
   imports: [],
   templateUrl: './ticket.component.html',
-  styleUrl: './ticket.component.css'
+  styleUrl: './ticket.component.css',
 })
 export class TicketComponent {
+  data = input.required<Ticket>();
+  detailsVisible = false;
+  markComplete = output();
 
+  toggleDetails() {
+    this.detailsVisible = !this.detailsVisible;
+  }
+
+  onMarkAsCompleted() {
+    this.markComplete.emit();
+  }
 }

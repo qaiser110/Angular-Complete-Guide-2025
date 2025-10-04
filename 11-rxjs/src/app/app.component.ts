e079a179150a6b65f549eda21e4eda0311dbf5fe
Qaiser <qaiser110@gmail.com>
2025-10-04 18:35:29 +1000

    Custom Observable

--- 11-rxjs/src/app/app.component.ts
+++ 11-rxjs/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
+  customInterval$ = new Observable((subscriber) => {
+    setInterval(() => {
+      subscriber.next({ message: 'Hello Observable!' });
+    }, 2000);
+  });
 
+  ngOnInit() {
+    const subscription = this.customInterval$.subscribe({
+      next: (val) => console.log(val),
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }

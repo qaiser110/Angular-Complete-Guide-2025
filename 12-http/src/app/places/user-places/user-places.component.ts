338e13f0c65b6ced4b14dbb2e953bf8c302aa866
Qaiser <qaiser110@gmail.com>
2025-10-06 02:35:33 +1100

    233 DELETE Requests

--- 12-http/src/app/places/user-places/user-places.component.ts
+++ 12-http/src/app/places/user-places/user-places.component.ts
@@ -37,4 +37,12 @@ export class UserPlacesComponent {
       subscription.unsubscribe();
     });
   }
+
+  onRemovePlace(place: Place) {
+    const subscription = this.placesService.removeUserPlace(place).subscribe();
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }

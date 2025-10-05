601d4c5bfbbb3b087d00ca52f22400a0a3054498
Qaiser <qaiser110@gmail.com>
2025-10-05 19:38:07 +1100

    227 Refactor HTTP Request Logic Into A Service

--- 12-http/src/app/places/available-places/available-places.component.ts
+++ 12-http/src/app/places/available-places/available-places.component.ts
@@ -5,6 +5,7 @@ import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs';
+import { PlacesService } from '../places.service';
 
 @Component({
   selector: 'app-available-places',
@@ -17,39 +18,39 @@ export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
   isFetching = signal(false);
   error = signal('');
-  private httpClient = inject(HttpClient);
+  private placesService = inject(PlacesService);
   private destroyRef = inject(DestroyRef);
 
   ngOnInit() {
     this.isFetching.set(true);
-    const subscription = this.httpClient
-      .get<{ places: Place[] }>('http://localhost:3000/places')
-      .pipe(map(({ places }) => places))
-      .subscribe({
-        next: (places) => {
-          this.places.set(places);
-        },
-        error: (err) => {
-          console.log(err);
-          this.error.set('Something went wrong fetching the places!');
-        },
-        complete: () => {
-          this.isFetching.set(false);
-        },
-      });
+    const subscription = this.placesService.loadAvailablePlaces().subscribe({
+      next: (places) => {
+        this.places.set(places);
+      },
+      error: (err) => {
+        this.error.set(err.message);
+      },
+      complete: () => {
+        this.isFetching.set(false);
+      },
+    });
 
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
     });
   }
 
-  onSelectPlace(place: Place) {
-    this.httpClient
-      .put('http://localhost:3000/user-places', { placeId: place.id })
+  onSelectPlace(selectedPlace: Place) {
+    const subscription = this.placesService
+      .addPlaceToUserPlaces(selectedPlace.id)
       .subscribe({
         next: (resData) => {
           console.log(resData);
         },
       });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
   }
 }
--- 12-http/src/app/places/user-places/user-places.component.ts
+++ 12-http/src/app/places/user-places/user-places.component.ts
@@ -4,7 +4,8 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
 import { PlacesComponent } from '../places.component';
 import { Place } from '../place.model';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
+import { catchError, map, throwError } from 'rxjs';
+import { PlacesService } from '../places.service';
 
 @Component({
   selector: 'app-user-places',
@@ -17,26 +18,22 @@ export class UserPlacesComponent {
   places = signal<Place[] | undefined>(undefined);
   isFetching = signal(false);
   error = signal('');
-  private httpClient = inject(HttpClient);
+  private placesService = inject(PlacesService);
   private destroyRef = inject(DestroyRef);
 
   ngOnInit() {
     this.isFetching.set(true);
-    const subscription = this.httpClient
-      .get<{ places: Place[] }>('http://localhost:3000/user-places')
-      .pipe(map(({ places }) => places))
-      .subscribe({
-        next: (places) => {
-          this.places.set(places);
-        },
-        error: (err) => {
-          console.log(err);
-          this.error.set('Error fetching you favorite places!');
-        },
-        complete: () => {
-          this.isFetching.set(false);
-        },
-      });
+    const subscription = this.placesService.loadUserPlaces().subscribe({
+      next: (places) => {
+        this.places.set(places);
+      },
+      error: (error) => {
+        this.error.set(error.message);
+      },
+      complete: () => {
+        this.isFetching.set(false);
+      },
+    });
 
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();

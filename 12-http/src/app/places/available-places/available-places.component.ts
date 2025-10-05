86ecdddae595e6f0415737071d2ffd6de3889776
Qaiser <qaiser110@gmail.com>
2025-10-05 14:53:55 +1100

    Loading Spinner/Text

--- 12-http/src/app/places/available-places/available-places.component.ts
+++ 12-http/src/app/places/available-places/available-places.component.ts
@@ -15,17 +15,34 @@ import { map } from 'rxjs';
 })
 export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
   private httpClient = inject(HttpClient);
 
   ngOnInit() {
+    this.isFetching.set(true);
     this.httpClient
       .get<{ places: Place[] }>('http://localhost:3000/places', {
         observe: 'body',
         responseType: 'json',
       })
       .pipe(map(({ places }) => places as Place[]))
-      .subscribe((places) => {
-        this.places.set(places);
+      .subscribe({
+        next: (places) => {
+          this.places.set(places);
+        },
+        complete: () => {
+          this.isFetching.set(false);
+        },
+      });
+  }
+
+  onSelectPlace(place: Place) {
+    this.httpClient
+      .put('http://localhost:3000/user-places', { placeId: place.id })
+      .subscribe({
+        next: (resData) => {
+          console.log(resData);
+        },
       });
   }
 }

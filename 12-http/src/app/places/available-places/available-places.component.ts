0cba0324fe6821e1a050ce5cb57dcde86e54e7f7
Qaiser <qaiser110@gmail.com>
2025-10-05 12:07:53 +1100

    HTTPClient GET available-spaces

--- 12-http/src/app/places/available-places/available-places.component.ts
+++ 12-http/src/app/places/available-places/available-places.component.ts
@@ -1,8 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -11,6 +13,19 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  private httpClient = inject(HttpClient);
+
+  ngOnInit() {
+    this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/places', {
+        observe: 'body',
+        responseType: 'json',
+      })
+      .pipe(map(({ places }) => places as Place[]))
+      .subscribe((places) => {
+        this.places.set(places);
+      });
+  }
 }

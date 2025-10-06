1a431af77b565abbc611467c68f92ba74727b4b0
Qaiser <qaiser110@gmail.com>
2025-10-06 16:53:33 +1100

    Login Form Validation

--- 13-forms/src/app/auth/login/login.component.ts
+++ 13-forms/src/app/auth/login/login.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  imports: [FormsModule],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  onSubmit(formData: NgForm) {
+    if (!formData.valid) {
+      return;
+    }
+
+    const enteredEmail = formData.form.value.email;
+    const enteredPassword = formData.form.value.password;
+
+    console.log(formData.form);
+    console.log(enteredEmail, enteredPassword);
+
+    formData.form.reset();
+  }
+}

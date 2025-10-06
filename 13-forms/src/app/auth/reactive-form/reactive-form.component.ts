44a6785068dba8806391bcf5aaab4122738f098b
Qaiser <qaiser110@gmail.com>
2025-10-06 21:18:47 +1100

    247 Reactive Form

--- 13-forms/src/app/auth/reactive-form/reactive-form.component.ts
+++ 13-forms/src/app/auth/reactive-form/reactive-form.component.ts
@@ -1,9 +1,47 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-login',
   standalone: true,
   templateUrl: './reactive-form.component.html',
   styleUrl: './reactive-form.component.css',
+  imports: [ReactiveFormsModule],
 })
-export class ReactiveLoginComponent {}
+export class ReactiveLoginComponent {
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)],
+    }),
+  });
+
+  get invalidEmail() {
+    return (
+      this.form.controls.email.touched &&
+      this.form.controls.email.dirty &&
+      this.form.controls.email.invalid
+    );
+  }
+  get invalidPassword() {
+    return (
+      this.form.controls.password.touched &&
+      this.form.controls.password.dirty &&
+      this.form.controls.password.invalid
+    );
+  }
+
+  onSubmit() {
+    console.log(this.form);
+    const enteredEmail = this.form.value.email;
+    const enteredPassword = this.form.value.password;
+    console.log(enteredEmail, enteredPassword);
+  }
+}

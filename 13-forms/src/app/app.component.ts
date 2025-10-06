df4f89dc8b19ab2658e1745a48bf9065be93579a
Qaiser <qaiser110@gmail.com>
2025-10-06 19:05:31 +1100

    Initial code for Reactive Login form

--- 13-forms/src/app/app.component.ts
+++ 13-forms/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { LoginComponent } from './auth/login/login.component';
+import { ReactiveLoginComponent } from './auth/reactive-form/reactive-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [LoginComponent],
+  imports: [LoginComponent, ReactiveLoginComponent],
 })
 export class AppComponent {}
---
+++ 13-forms/src/app/auth/reactive-form/reactive-form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reactive-login',
+  standalone: true,
+  templateUrl: './reactive-form.component.html',
+  styleUrl: './reactive-form.component.css',
+})
+export class ReactiveLoginComponent {}
